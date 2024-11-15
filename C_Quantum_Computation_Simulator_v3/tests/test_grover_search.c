e975ff281528683c98e08776d9136e7bfa9d0543
MarioSavu <mariosavu256@gmail.com>
2024-11-15 21:49:56 +0200

    Adding reference wasm 2.0/3.0 codes, used to test this also, which give same results

--- C_Quantum_Computation_Simulator_v3/tests/test_grover_search.c
+++ C_Quantum_Computation_Simulator_v3/tests/test_grover_search.c
@@ -57,3 +57,151 @@ int main() {
     return 0;
 }
 
+/*
+QASM 2.0 reference:
+OPENQASM 2.0;
+include "qelib1.inc";
+
+qreg q[3];
+
+
+x q[2];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+h q[0];
+h q[1];
+h q[2];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+x q[0];
+ccx q[0], q[1], q[2];
+x q[0];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+h q[0];
+h q[1];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+x q[1];
+x q[0];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+h q[1];
+cx q[0], q[1];
+h q[1];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+x q[1];
+x q[0];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+h q[0];
+h q[1];
+h q[2];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+
+x q[0];
+ccx q[0], q[1], q[2];
+x q[0];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+h q[0];
+h q[1];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+x q[1];
+x q[0];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+h q[1];
+cx q[0], q[1];
+h q[1];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+x q[1];
+x q[0];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+h q[0];
+h q[1];
+h q[2];
+barrier q[1];
+barrier q[0];
+barrier q[2];
+*/
+
+/*
+QASM 3.0 reference:
+OPENQASM 3;
+include 'customgates.inc';
+
+bit[3] c;
+qubit[3] q;
+
+x q[2];
+barrier;
+h q[0];
+h q[1];
+h q[2];
+barrier;
+x q[0];
+ccx q[0], q[1], q[2];
+x q[0];
+barrier;
+h q[0];
+h q[1];
+barrier;
+x q[1];
+x q[0];
+barrier;
+h q[1];
+cx q[0], q[1];
+h q[1];
+barrier;
+x q[1];
+x q[0];
+barrier;
+h q[0];
+h q[1];
+h q[2];
+barrier;
+x q[0];
+ccx q[0], q[1], q[2];
+x q[0];
+barrier;
+h q[0];
+h q[1];
+barrier;
+x q[1];
+x q[0];
+barrier;
+h q[1];
+cx q[0], q[1];
+h q[1];
+barrier;
+x q[1];
+x q[0];
+barrier;
+h q[0];
+h q[1];
+h q[2];
+barrier;
+
+c[0] = measure q[0];
+c[1] = measure q[1];
+c[2] = measure q[2];
+*/
